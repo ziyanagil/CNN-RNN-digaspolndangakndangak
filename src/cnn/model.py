d02b742b60603e0bd4d80a289c3a9d8269def0a8
Kefas Kurnia Jonathan <kefaskj@gmail.com>
2026-05-09 18:56:13 +0700

    feat(cnn): implemented model to load from keras

--- src/cnn/model.py
+++ src/cnn/model.py
@@ -9,8 +9,18 @@ class CNNScratch:
         self.layers.append(layer)
 
     def load_from_keras(self, keras_model) -> None:
-        # TODO: iterasi self.layers dan panggil layer.load_weights(keras_layer)
-        raise NotImplementedError
+        scratch_idx = 0
+        for keras_layer in keras_model.layers:
+            cls = type(keras_layer).__name__
+            if cls in ('InputLayer', 'Dropout', 'Rescaling'):
+                continue
+                
+            if cls in ('Conv2D', 'LocallyConnected2D', 'MaxPooling2D', 'AveragePooling2D', 'Dense'):
+                if scratch_idx < len(self.layers) and hasattr(self.layers[scratch_idx], 'load_weights'):
+                    self.layers[scratch_idx].load_weights(keras_layer)
+                scratch_idx += 1
+            elif cls in ('Flatten', 'GlobalAveragePooling2D', 'GlobalMaxPooling2D'):
+                scratch_idx += 1
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         out = x
